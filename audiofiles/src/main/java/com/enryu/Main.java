17b419f191538201d73b34a7421b492b31b278ed
Oliver Williams <104966737+ShaD0wbUrn142@users.noreply.github.com>
2026-02-01 12:02:23 +1000

    Done!

---
+++ audiofiles/src/main/java/com/enryu/Main.java
@@ -0,0 +1,10 @@
+package com.enryu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Creating a WAV file...");
+        wav w = new wav();
+        w.main();
+        System.out.println("Program ended.");
+    }
+}
